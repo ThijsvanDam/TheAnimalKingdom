88dcfec6530c1c48a5cbafd42533cd288723bdd9
Sander Bos <sanderbos17@gmail.com>
2018-02-21 11:43:43 +0100

    Added DBPanel

--- TheAnimalKingdom/Form1.cs
+++ TheAnimalKingdom/Form1.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TheAnimalKingdom.Util;
 
 namespace TheAnimalKingdom
 {
     public partial class Form1 : Form
     {
+        private World _world;
+        
         public Form1()
         {
             InitializeComponent();
+            
+            _world = new World();
+        }
+        
+        private void _dbPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            //ToDo: Implement
+        }
+        
+        private void _dbPanel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //ToDo: Implement
         }
     }
 }
